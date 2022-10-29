1a19c547aec05f8a6998233bc2df9009d91cf1f5
grahammauer <98790716+grahammauer@users.noreply.github.com>
2022-10-29 12:58:58 -0600

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Here are some common constants that are used throughough physics and math
+
+class constants:
+	
+	# Math Constants
+	pi  = 3.14159265358979
+	phi = 1.61803398874989
+	tau = 6.28318530717959
+	e   = 2.71828182845904
+	
+	# Physics Constants
+	G   = 6.67430e-11      # Gravitational Constant [m^{3} kg^{-1} s^{-2}]
+	e_0 = 8.8541878128e-12 # Vacuum Permitivity [s^{4} A^{2} m^{-3} kg^{-1}]
+
