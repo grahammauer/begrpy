977b8014b80a72e8095276274f1c5214863e2fae
grahammauer <98790716+grahammauer@users.noreply.github.com>
2022-12-23 15:53:46 -0600

    Update LinearAlgebra.py

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -75,6 +75,7 @@ def vectorDotProduct(vector1, vector2):
 
 	return matrixMultiply(vector1, matrixTranspose(vector2))
 
+# Row Echelon Form
 def forwardSolve(matrix):
     	'''
     	Row reduces a dense matrix to an upper triangular matrix in O(n^3) time. 
@@ -115,3 +116,70 @@ def forwardSolve(matrix):
 				matrix[r+count+1][c] = matrix[r+count+1][c] - m * matrix[count][c]
 
 	return matrix
+
+# Row Echelon form to Reduced Row Echelon Form
+def backwardSolve(matrix):
+	'''
+	Take an upper triangular matrix and reduce to row echelon form. 
+
+	Assumptions
+	-----------
+	- Matrix is in row reduced form
+	- Matrix of size (n, n+1)
+	- No '0' rows or columns
+
+	Parameters
+	----------
+	matrix : list
+		(m x n) size 2-D list following assumptions above
+
+	Returns
+	-------
+	Row echelon form of the matrix
+
+	NOTE:
+	-----
+	- Weird '-0.0' output in the lower triangular portion
+	'''
+
+	# Identify the dimensions of the matrix
+	rows = len(matrix)
+	columns = len(matrix[0])
+
+	# Make the diagonal 1
+
+	for r in range(rows):
+
+		m = matrix[r][r]
+
+		for c in range(columns):
+
+			matrix[r][c] = matrix[r][c] / m
+
+	# Because n, n+1 matrix, omit the first column, also omit last column
+	for c in range(columns - 2):
+
+		# Start c at the second to last column
+		c = -1 * (c + 2) 
+
+		# Iterates through each row above the pivot position
+		for r in range(rows + 1 + c):
+
+			# Make sure the last column changes. Everything else will be 0
+			matrix[r][-1] = matrix[r][-1] - matrix[c + 1][-1] * matrix[r][c]
+
+	# Make other entries besides diagonal and right column 0
+	for c in range(columns - 2):
+
+		for r in range(c + 1):
+
+			matrix[r][c + 1] = 0.0
+
+	return matrix
+
+# Gauss Jordan Elimination
+def GaussJordanElim(matrix):
+
+	gaussJordan = backwardSolve(forwardSolve(matrix))
+
+	return gaussJordan
